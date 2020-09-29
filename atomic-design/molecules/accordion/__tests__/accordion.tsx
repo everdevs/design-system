5a056002422dc2bf0c6b7d8926492ee8e96791a0
Adrian Payne <definitive@outlook.com>
2020-09-29 09:08:46 +0200

    test: add Wrapper from dev-helpers

--- atomic-design/molecules/accordion/__tests__/accordion.tsx
+++ atomic-design/molecules/accordion/__tests__/accordion.tsx
@@ -1,14 +1,15 @@
-import { withTheme, createTree } from "@evernest/dev-helpers";
-import { theme } from "@evernest/theme";
+import { Wrapper, createTree } from "@evernest/dev-helpers";
 import { mount } from "enzyme";
 import React from "react";
-import { Accordion, AccordionProps, StyledPanel, StyledButton } from "../src";
-
-const ThemedAccordion = withTheme<AccordionProps>(Accordion, theme);
+import { Accordion, StyledPanel, StyledButton } from "../src";
 
 test("Accordion renders title", () => {
 	const text = "Title";
-	const wrapper = mount(<ThemedAccordion title="Title" />);
+	const wrapper = mount(
+		<Wrapper>
+			<Accordion title="Title" />
+		</Wrapper>
+	);
 	expect(wrapper.find("[data-test-id='styled-inner-button-wrapper-label']").at(0).text()).toEqual(
 		text
 	);
@@ -16,17 +17,29 @@ test("Accordion renders title", () => {
 
 test("Accordion renders children", () => {
 	const text = "Hello World!";
-	const wrapper = mount(<ThemedAccordion title="Title">{text}</ThemedAccordion>);
+	const wrapper = mount(
+		<Wrapper>
+			<Accordion title="Title">{text}</Accordion>
+		</Wrapper>
+	);
 	expect(wrapper.find(StyledPanel).text()).toEqual(text);
 });
 
 test("Accordion matches snapshot", () => {
-	const closed = createTree(<ThemedAccordion title="Title">content</ThemedAccordion>);
+	const closed = createTree(
+		<Wrapper>
+			<Accordion title="Title">content</Accordion>
+		</Wrapper>
+	);
 	expect(closed).toMatchSnapshot();
 });
 
 test("Accordion prop 'expanded' toggles on click", () => {
-	const wrapper = mount(<ThemedAccordion title="Title" />);
+	const wrapper = mount(
+		<Wrapper>
+			<Accordion title="Title" />
+		</Wrapper>
+	);
 	const button = wrapper.find(StyledButton);
 
 	button.simulate("click");
