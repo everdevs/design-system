10fcc138bf43bd55aceb1c3dcef0d207c2cf4684
Adrian Payne <definitive@outlook.com>
2020-10-20 10:35:27 +0200

    feat(page): add flex properties to main

--- atomic-design/templates/page/src/main.tsx
+++ atomic-design/templates/page/src/main.tsx
@@ -2,5 +2,8 @@ import styled from "@emotion/styled";
 import { PropsWithTheme } from "@evernest/theme";
 
 export const Main = styled.main<PropsWithTheme>`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
 	min-height: calc(100vh - var(--header-height-ref));
 `;
